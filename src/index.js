4fe14cf4fbebea6290ec16ba2a3a06600e7af070
Tatsiana Sakalinskaya <tani4ik@gmail.com>
2018-09-29 18:40:33 +0300

    implemented brackets without special cases

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let isOpening = isOpeningBracket(str[i], bracketsConfig);
+    if (isOpening) {
+      stack.push(str[i]);
+    }
+    else {
+      let lastOpeningBracket = stack.pop();
+      if (getOpeningBracket(str[i], bracketsConfig) !== lastOpeningBracket) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+} 
+
+function isOpeningBracket(symbol, bracketsConfig) {
+  for (let i = 0; i < bracketsConfig.length; i++) {
+      if (symbol === bracketsConfig[i][0]) {
+          return true;
+      }
+  }
+return false;
+}
+
+function getOpeningBracket (closingBracket, bracketsConfig) {
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    if (closingBracket === bracketsConfig[i][1]) {
+      return bracketsConfig[i][0];
+    }
+  }
 }
