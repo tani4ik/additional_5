f10fdac1a0e5494471f9fe9874499d8ce23d0854
Tatsiana Sakalinskaya <tani4ik@gmail.com>
2018-09-30 20:46:20 +0300

    Implemented brackets for equal brackets

--- src/index.js
+++ src/index.js
@@ -2,34 +2,35 @@ module.exports = function check(str, bracketsConfig) {
   let stack = [];
 
   for (let i = 0; i < str.length; i++) {
-    let isOpening = isOpeningBracket(str[i], bracketsConfig);
-    if (isOpening) {
-      stack.push(str[i]);
-    }
-    else {
-      let lastOpeningBracket = stack.pop();
-      if (getOpeningBracket(str[i], bracketsConfig) !== lastOpeningBracket) {
-        return false;
+    let symbol = str[i];
+    let bracketsFromConfig = getBracketsFromConfig(symbol, bracketsConfig);
+
+    if (bracketsFromConfig[0] !== bracketsFromConfig[1]) {
+      if (symbol === bracketsFromConfig[0]) {
+        stack.push(symbol);
+      } else {
+        let lastOpeningBracket = stack.pop();
+        if (bracketsFromConfig[0] !== lastOpeningBracket) {
+          return false;
+        }
+      }
+    } else {
+      let topOfTheStack = stack.length > 0 ? stack[stack.length - 1] : undefined;
+      if (topOfTheStack !== symbol) {
+        stack.push(symbol);
+      } else {
+        stack.pop();
       }
     }
   }
 
   return stack.length === 0;
-} 
-
-function isOpeningBracket(symbol, bracketsConfig) {
-  for (let i = 0; i < bracketsConfig.length; i++) {
-      if (symbol === bracketsConfig[i][0]) {
-          return true;
-      }
-  }
-return false;
 }
 
-function getOpeningBracket (closingBracket, bracketsConfig) {
+function getBracketsFromConfig(bracketSymbol, bracketsConfig) {
   for (let i = 0; i < bracketsConfig.length; i++) {
-    if (closingBracket === bracketsConfig[i][1]) {
-      return bracketsConfig[i][0];
+    if (bracketSymbol === bracketsConfig[i][0] || bracketSymbol === bracketsConfig[i][1]) {
+      return bracketsConfig[i];
     }
   }
 }
